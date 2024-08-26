54d08ee2ea72e2c4f0aea23410f17126b9c9145b
akshay.thakar@smartbear.com <akshay.thakar@smartbear.com>
2024-08-26 18:17:34 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight-local.com:8443/api/v1/projects/cHJqOjI3/spectral.js?branch_slug=main&token=e868cd44-3bb7-4828-a5a7-5828ee65ddda";
+export default { extends: ruleset };
